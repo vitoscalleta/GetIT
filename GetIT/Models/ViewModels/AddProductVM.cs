14ddb8d754614625f046b361871da2ac3996a109
Hari <infy_723012@outlook.com>
2020-08-16 20:11:24 +0530

    Added Display Names for AddProduct VM, Fixed SubCategory List empty issue in Add Product page, Fixed alignmen issue in product list page.

--- GetIT/Models/ViewModels/AddProductVM.cs
+++ GetIT/Models/ViewModels/AddProductVM.cs
@@ -1,24 +1,26 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace GetIT.Models.ViewModels
 {
     public class AddProductVM
     {
         [Required]
+        [Display(Name="Product Name")]
         public string ProductName { get; set; }
         [Required]
         public string Description { get; set; }
         [Required]
         public double Price { get; set; }
-
+        [Display(Name = "Product Category")]
         public int ProductCategory { get; set; }
-
+        
         public List<string> ProductCategoryList { get; set; }
-
+        [Display(Name = "Product Sub-Category")]
         public int ProductSubCategory { get; set; }
-
+        
         public List<string> ProductSubCategoryList { get; set; }
 
         public IFormFile Images { get; set; }
